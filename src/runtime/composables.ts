fbedaac59d6c4dd6225f2ea2c9a9c30974cfb6a3
Harlan Wilton <harlan@harlanzw.com>
2022-10-04 02:54:31 +1100

    fix: imports

--- src/runtime/composables.ts
+++ src/runtime/composables.ts
@@ -2,6 +2,7 @@ import type { MaybeComputedRef } from '@vueuse/shared'
 import type { MetaFlatRef } from '@zhead/vue'
 import { unpackMeta } from '@zhead/vue'
 import type { MetaObject } from '@nuxt/schema'
+import { ref, watchEffect } from 'vue'
 import { useNuxtApp } from '#app'
 
 export function useMetaTags(metaTags: MaybeComputedRef<MetaFlatRef>) {

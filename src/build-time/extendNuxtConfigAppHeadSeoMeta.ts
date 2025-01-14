92e7854c3f25f9b701fccc1fbdbebc7364fcd148
harlan <harlan@harlanzw.com>
2025-01-14 13:34:02 +1100

    fix: `seo.meta` not being parsed
    Fixes #35

--- src/build-time/extendNuxtConfigAppHeadSeoMeta.ts
+++ src/build-time/extendNuxtConfigAppHeadSeoMeta.ts
@@ -1,27 +1,25 @@
 import type { Nuxt } from '@nuxt/schema'
-import type { UseHeadInput } from 'unhead'
 import { useNuxt } from '@nuxt/kit'
 import { unpackMeta } from '@unhead/shared'
 
-export default function extendNuxtConfigAppHeadSeoMeta(nuxt: Nuxt = useNuxt()): UseHeadInput<any> {
+export default function extendNuxtConfigAppHeadSeoMeta(nuxt: Nuxt = useNuxt()) {
   // from the root public let's find which ones we can infer SEO meta and link tags from, following the Next.js convention
   // outlined in this site: https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons
-  // nuxt.options.app.seoMeta  is deprecated due to type complexities
+  // nuxt.options.app.seoMeta is deprecated due to type complexities
   // these other modes are deprecated because the tpyes are too difficult
   const seoMeta = nuxt.options.app?.seoMeta || nuxt.options.app?.head?.seoMeta || false
   const configSeoMeta = nuxt.options.seo?.meta || false
-  if (!seoMeta)
+  if (!seoMeta && !configSeoMeta)
     return
   nuxt.options.app.head = nuxt.options.app.head || {}
   nuxt.options.app.head = {
     ...nuxt.options.app.head,
     meta: [
       ...nuxt.options.app.head.meta || [],
-      ...unpackMeta(seoMeta),
-      ...unpackMeta(configSeoMeta),
+      ...unpackMeta(seoMeta || {}),
+      ...unpackMeta(configSeoMeta || {}),
     ],
   }
   delete nuxt.options.app.seoMeta
   delete nuxt.options.app.head.seoMeta
-  return nuxt.options.app.head as any as UseHeadInput<any>
 }

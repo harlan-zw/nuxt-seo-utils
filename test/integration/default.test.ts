3da47f690df68ca29afd1acb423db3f183d514e6
harlan <harlan@harlanzw.com>
2025-04-02 16:20:10 +1100

    chore: broken snapshot

--- test/integration/default.test.ts
+++ test/integration/default.test.ts
@@ -34,8 +34,8 @@ describe('default', () => {
       <meta name="description" content="Hi, welcome to the test v1.3.4 of en name.">
       <meta property="og:type" content="website">
       <meta name="twitter:card" content="summary_large_image">
-      <meta property="og:title" data-infer="true" content="Home x en name">
-      <meta property="og:description" data-infer="true" content="Hi, welcome to the test v1.3.4 of en name.">
+      <meta property="og:title" data-infer="" content="Home x en name">
+      <meta property="og:description" data-infer="" content="Hi, welcome to the test v1.3.4 of en name.">
       <meta property="og:url" content="https://nuxtseo.com/">
       <meta property="og:site_name" content="en name">"
     `)

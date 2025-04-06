5e37281ee19fe381bb1180a789a0307d654a9b94
harlan <harlan@harlanzw.com>
2025-04-06 17:05:50 +1000

    chore: bump unhead for broken inferseometa

--- src/runtime/app/plugins/inferSeoMetaPlugin.ts
+++ src/runtime/app/plugins/inferSeoMetaPlugin.ts
@@ -1,6 +1,7 @@
 import { injectHead } from '#imports'
 import { InferSeoMetaPlugin } from '@unhead/addons'
 import { defineNuxtPlugin } from 'nuxt/app'
+import { TemplateParamsPlugin } from 'unhead/plugins'
 
 export default defineNuxtPlugin(() => {
   const head = injectHead()
@@ -9,5 +10,6 @@ export default defineNuxtPlugin(() => {
   if (!head)
     return
 
+  head.use(TemplateParamsPlugin)
   head.use(InferSeoMetaPlugin())
 })

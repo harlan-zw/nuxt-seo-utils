b16b2ad03374452578f37798bf2bec7f918dbd76
Harlan Wilton <harlan@harlanzw.com>
2025-04-16 16:44:34 +1000

    chore: bump & lint

--- src/runtime/app/plugins/defaults.ts
+++ src/runtime/app/plugins/defaults.ts
@@ -1,6 +1,4 @@
-import { useSiteConfig } from '#site-config/app/composables/useSiteConfig'
 import { defineNuxtPlugin } from 'nuxt/app'
-import { ref } from 'vue'
 import { applyDefaults } from '../logic/applyDefaults'
 
 export default defineNuxtPlugin({
